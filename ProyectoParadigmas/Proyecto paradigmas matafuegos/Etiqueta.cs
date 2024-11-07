ffd908e94ecad989587c438ef8106074ac0eaf32
Zeindesito <matiasjoelcasa@hotmail.com>
2024-11-07 19:59:31 -0300

    Subiendo prueba

---
+++ ProyectoParadigmas/Proyecto paradigmas matafuegos/Etiqueta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_paradigmas_matafuegos
+{
+    public class Etiqueta
+    {
+        public DateTime FechaRevision { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public Matafuego Matafuego_ { get; set; }
+
+        public Etiqueta() { }
+
+        public void Rellenar(DateTime fechaRevision, DateTime fechaVencimiento, Matafuego matafuego)
+        {
+            FechaRevision = fechaRevision;
+            FechaVencimiento = fechaVencimiento;
+            Matafuego_ = matafuego;
+        }
+    }
+}
---
+++ ProyectoParadigmas/Proyecto paradigmas matafuegos/Matafuego.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_paradigmas_matafuegos
+{
+    public abstract class Matafuego
+    {
+        public int Kilos {  get; set; }
+        public Etiqueta EtiquetaMatafuego { get; set; }
+        public string Arosello { get; set; }
+        public double Gas { get; set; }
+
+        public void RellenarGas(double cantidadGas)
+        {
+            Gas = cantidadGas;
+        }
+
+
+    }
+}
